a7a960d8dd4e59cdedc85f126ce745531e3905fa
vishnuvarthan <vishnuvarthan7400@gmail.com>
2025-12-02 11:51:27 +0530

    fix optimization

--- ex-1.py
+++ ex-1.py
@@ -1,15 +1,8 @@
+r = int(input("Enter number of rows: "))
+c = int(input("Enter number of columns: "))
 
-array=[]
-r=int(input("enter the row value:"))
-c=int(input("enter the column value:"))
-for i in range(r):
-    row=[]
-    for j in range(c):
-        value=int(input(f"enter the value:({i},{j})"))
-        row.append(value)
-    array.append(row)
-    
-    print("answer")
-print(array)
+array = [[int(input(f"Enter value ({i},{j}): ")) for j in range(c)] for i in range(r)]
 
-        
+print("\nAnswer:")
+for row in array:
+    print(row)
