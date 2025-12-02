69d3e17ed87d85073e48d837d85c24c2fce79fee
SanjayR14 <sanjusaran1435@gmal.com>
2025-12-02 11:33:12 +0530

    first_commit

---
+++ ex-1.py
@@ -0,0 +1,14 @@
+
+array=[]
+r=int(input("enter the row value:"))
+c=int(input("enter the column value:"))
+for i in range(r):
+    row=[]
+    for j in range(c):
+        value=int(input(f"enter the value:({i},{j})"))
+        row.append(value)
+    array.append(row)
+    
+print(array)
+
+        
